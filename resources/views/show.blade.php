e92dffc15578c07078bd7c1992baf3cfcea18498
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-06-24 22:23:14 -0400

    add styles to Task Page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,42 +3,53 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{$task->description}}</p>
+    <div class="mb-4">
+        <a href="{{route('tasks.index')}}" class="link">
+            ← Back to Task List
+        </a>
+    </div>
+
+    <p class="mb-4 text-slate-700">{{$task->description}}</p>
 
     @if($task->long_description)
-        <p>{{$task->long_description}}</p>
+        <p class="mb-4 text-slate-700">{{$task->long_description}}</p>
     @endif
 
-    <p>{{$task->created_at}}</p>
-    <p>{{$task->updated_at}}</p>
-    <p>
+    <p class="mb-4 text-sm text-slate-500">Created: {{$task->created_at->diffForHumans()}}
+        | Updated: {{$task->updated_at->diffForHumans()}}</p>
+
+    <p class="mb-4">
         @if($task->completed)
+            <span class="font-medium text-green-500">
             Completed
+            </span>
         @else
+            <span class="font-medium text-red-500">
             Not Completed
+            </span>
         @endif
     </p>
 
 
-    <div>
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
-    </div>
-
-    <div>
+    <div class="flex gap-2">
+        {{-- Edit Button --}}
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}"
+           class="btn"
+        >Edit</a>
+        {{-- Toggle Button --}}
         <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
             @csrf
             @method('PUT')
-            <button type="submit">
+            <button type="submit" class="btn">
                 Mark {{$task->completed ? 'Not Completed' : 'Completed'}}
             </button>
         </form>
-    </div>
-
-    <div>
+        {{-- Delete Button --}}
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit">DELETE</button>
+            <button type="submit" class="btn">Delete</button>
         </form>
     </div>
+
 @endsection
